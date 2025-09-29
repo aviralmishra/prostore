3d41d7fc46562a5a1c832fdbd6cc138ea9d0836b
Aviral Mishra <aviral.mishra@gmail.com>
2025-09-29 14:55:59 +1000

    toggle mode

--- components/shared/header/index.tsx
+++ components/shared/header/index.tsx
@@ -3,6 +3,7 @@ import Image from 'next/image';
 import Link from 'next/link';
 import { Button } from '@/components/ui/button';
 import { APP_NAME } from '@/lib/constants';
+import ModeToggle from './mode-toggle';
 
 const Header = () => {
     return (
@@ -26,6 +27,7 @@ const Header = () => {
                     </Link>
                 </div>
                 <div className="space-x-2">
+                    <ModeToggle></ModeToggle>
                     <Button
                         asChild
                         variant="ghost"
