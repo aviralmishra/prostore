073e5201e793b6a6ecfc6842794962934703bdca
Aviral Mishra <aviral.mishra@gmail.com>
2025-10-01 09:44:20 +1000

    product list

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,5 +1,14 @@
+import ProductList from '@/components/shared/product/product-list';
+import sampleData from '@/db/sample-data';
+
 const HomePage = async () => {
-    return <>Prostore</>;
+    return (
+        <ProductList
+            data={sampleData.products}
+            title="Newest Arrivals..."
+            limit={4}
+        ></ProductList>
+    );
 };
 
 export default HomePage;
