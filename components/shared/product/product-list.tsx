3351a9de1dcbb45a525ba8891d32a6541b1e2cbf
Aviral Mishra <aviral.mishra@gmail.com>
2025-10-03 08:59:00 +1000

    types validation

--- components/shared/product/product-list.tsx
+++ components/shared/product/product-list.tsx
@@ -1,7 +1,8 @@
 import ProductCard from './product-card';
+import { Product } from '@/types';
 
 type ProductListProps = {
-    data: any;
+    data: Product[];
     title?: string;
 };
 
@@ -11,7 +12,7 @@ const ProductList = ({ data, title }: ProductListProps) => {
             <h2 className="h2-bold mb-4">{title}</h2>
             {data.length > 0 ? (
                 <div className="grid grid-col-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
-                    {data.map((product: any) => (
+                    {data.map((product: Product) => (
                         <ProductCard
                             key={product.slug}
                             product={product}
