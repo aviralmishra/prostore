028f9be35db4ae17940a65a853c88dcf3fef23ec
Aviral Mishra <aviral.mishra@gmail.com>
2025-10-03 15:16:24 +1000

    product details page

--- lib/actions/product.actions.ts
+++ lib/actions/product.actions.ts
@@ -4,6 +4,11 @@ import { prisma } from '@/db/prisma';
 import { convertToPlainObject } from '@/lib/utils';
 import { LATEST_PRODUCTS_LIMIT } from '../constants';
 
+/**
+ * Get latest products from database
+ *
+ * @returns products list
+ */
 export async function getLatestProducts() {
     const data = await prisma.product.findMany({
         take: LATEST_PRODUCTS_LIMIT,
@@ -11,4 +16,13 @@ export async function getLatestProducts() {
     });
 
     return convertToPlainObject(data);
+}
+
+/**
+ * Get products by slug
+ *
+ * @returns product
+ */
+export async function getProductBySlug(slug: string) {
+    return await prisma.product.findFirst({ where: { slug: slug } });
 }
