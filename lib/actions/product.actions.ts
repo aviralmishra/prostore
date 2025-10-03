9e17dfff890f7295ec9751c710b138676a1a59a4
Aviral Mishra <aviral.mishra@gmail.com>
2025-10-03 14:49:21 +1000

    prisma neon serverless integration

--- lib/actions/product.actions.ts
+++ lib/actions/product.actions.ts
@@ -1,12 +1,10 @@
 'use server';
 
+import { prisma } from '@/db/prisma';
 import { convertToPlainObject } from '@/lib/utils';
 import { LATEST_PRODUCTS_LIMIT } from '../constants';
-import { PrismaClient } from '../generated/prisma';
 
 export async function getLatestProducts() {
-    const prisma = new PrismaClient();
-
     const data = await prisma.product.findMany({
         take: LATEST_PRODUCTS_LIMIT,
         orderBy: { createdAt: 'desc' },
