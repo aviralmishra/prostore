9ce39e789c13b421cb87e8c017f7569c1ff5ae88
Aviral Mishra <aviral.mishra@gmail.com>
2025-10-08 13:26:23 +1100

    user seed data

--- db/seed.ts
+++ db/seed.ts
@@ -3,9 +3,20 @@ import sampleData from './sample-data';
 
 async function main() {
     const prisma = new PrismaClient();
+
+    // products
     await prisma.product.deleteMany();
 
+    // users
+    await prisma.account.deleteMany();
+    await prisma.session.deleteMany();
+    await prisma.verificationToken.deleteMany();
+    await prisma.user.deleteMany();
+
+    // inserts
     await prisma.product.createMany({ data: sampleData.products });
+    await prisma.user.createMany({ data: sampleData.users });
+
     console.log('The database has been seeded successfully.');
 }
 
