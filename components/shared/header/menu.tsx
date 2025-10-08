df0c81234cbbd15fded29361f5dcd144be8a6dd9
Aviral Mishra <aviral.mishra@gmail.com>
2025-10-08 15:53:22 +1100

    signed in user actions

--- components/shared/header/menu.tsx
+++ components/shared/header/menu.tsx
@@ -1,6 +1,7 @@
-import Link from 'next/link';
-
 import { Button } from '@/components/ui/button';
+import ModeToggle from './mode-toggle';
+import Link from 'next/link';
+import { EllipsisVertical, ShoppingCart } from 'lucide-react';
 import {
     Sheet,
     SheetContent,
@@ -8,57 +9,40 @@ import {
     SheetTitle,
     SheetTrigger,
 } from '@/components/ui/sheet';
-
-import { EllipsisVertical, ShoppingCart, UserIcon } from 'lucide-react';
-
-import ModeToggle from './mode-toggle';
+import UserButton from './user-button';
 
 const Menu = () => {
     return (
         <div className="flex justify-end gap-3">
             <nav className="hidden md:flex w-full max-w-xs gap-1">
-                <div className="space-x-2">
-                    <ModeToggle></ModeToggle>
-                    <Button
-                        asChild
-                        variant="ghost"
-                    >
-                        <Link href="/cart">
-                            <ShoppingCart />
-                            Cart
-                        </Link>
-                    </Button>
-                    <Button asChild>
-                        <Link href="/cart">
-                            <UserIcon />
-                            Sign In
-                        </Link>
-                    </Button>
-                </div>
+                <ModeToggle />
+                <Button
+                    asChild
+                    variant="ghost"
+                >
+                    <Link href="/cart">
+                        <ShoppingCart /> Cart
+                    </Link>
+                </Button>
+                <UserButton />
             </nav>
             <nav className="md:hidden">
                 <Sheet>
                     <SheetTrigger className="align-middle">
-                        <EllipsisVertical></EllipsisVertical>
+                        <EllipsisVertical />
                     </SheetTrigger>
                     <SheetContent className="flex flex-col items-start">
                         <SheetTitle>Menu</SheetTitle>
-                        <ModeToggle></ModeToggle>
+                        <ModeToggle />
                         <Button
                             asChild
                             variant="ghost"
                         >
                             <Link href="/cart">
-                                <ShoppingCart />
-                                Cart
+                                <ShoppingCart /> Cart
                             </Link>
                         </Button>
-                        <Button asChild>
-                            <Link href="/cart">
-                                <UserIcon />
-                                Sign In
-                            </Link>
-                        </Button>
+                        <UserButton />
                         <SheetDescription></SheetDescription>
                     </SheetContent>
                 </Sheet>
