4963a91f0d1faae75b7116c2dd3b9b27df0558f5
Aviral Mishra <aviral.mishra@gmail.com>
2025-10-21 15:02:58 +1100

    paypal tests

--- tests/paypal.test.ts
+++ tests/paypal.test.ts
@@ -1,8 +1,34 @@
-import { generateAccessToken } from '../lib/paypal';
+import { generateAccessToken, paypal } from '../lib/paypal';
 
 test('generates access token from paypal', async () => {
     const tokenResponse = await generateAccessToken();
 
     expect(typeof tokenResponse).toBe('string');
     expect(tokenResponse.length).toBeGreaterThan(0);
+});
+
+test('creates a paypal order', async () => {
+    const price = 10.0;
+
+    const orderResponse = await paypal.createOrder(price);
+    console.log(orderResponse);
+
+    expect(orderResponse).toHaveProperty('id');
+    expect(orderResponse).toHaveProperty('status');
+    expect(orderResponse.status).toBe('CREATED');
+});
+
+test('simulate payment capture for order', async () => {
+    const orderId = '100';
+
+    const mockCapturePayment = jest
+        .spyOn(paypal, 'capturePayment')
+        .mockResolvedValue({
+            status: 'COMPLETED',
+        });
+
+    const captureResponse = await paypal.capturePayment(orderId);
+    expect(captureResponse).toHaveProperty('status', 'COMPLETED');
+
+    mockCapturePayment.mockRestore();
 });
