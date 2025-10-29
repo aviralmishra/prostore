f7556bd100e537057cd493d5c31382aabf43b3e8
Aviral Mishra <aviral.mishra@gmail.com>
2025-10-29 13:14:33 +1100

    extend user session

--- types/next-auth.d.ts
+++ types/next-auth.d.ts
@@ -1,21 +1,28 @@
+import { DefaultSession } from 'next-auth';
+
 import 'next-auth';
 import 'next-auth/jwt';
 
-declare module 'next-auth' {
-    interface Session {
-        id: string;
+declare module 'next-auth/jwt' {
+    /** Returned by the `jwt` callback and `getToken`, when using JWT sessions */
+    interface JWT {
+        sub: string;
         role: string;
-    }
-
-    interface User {
-        id: string;
-        role: string;
+        name: string;
     }
 }
 
-declare module 'next-auth/jwt' {
-    interface JWT {
-        id: string;
+declare module 'next-auth' {
+    /**
+     * Returned by `useSession`, `getSession` and received as a prop on the `SessionProvider` React Context
+     */
+    interface Session {
+        user: {
+            role: string;
+        } & DefaultSession['user'];
+    }
+
+    interface User {
         role: string;
     }
 }
