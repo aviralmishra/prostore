06e3affa2282b0a74718f3433ee44d362ceb8ff0
Aviral Mishra <aviral.mishra@gmail.com>
2025-10-30 15:23:57 +1100

    constants

--- lib/constants/index.ts
+++ lib/constants/index.ts
@@ -1,11 +1,24 @@
+// App
 export const APP_NAME = process.env.NEXT_PUBLIC_APP_NAME || 'Prostore';
 export const APP_DESCRIPTION =
     process.env.NEXT_PUBLIC_APP_DESCRIPTION || 'Shopping redefined...';
 export const SERVER_URL =
     process.env.NEXT_PUBLIC_SERVER_URL || 'http://localhost:3000';
+
+// Payment
+export const PAYMENT_METHODS = process.env.PAYMENT_METHODS
+    ? process.env.PAYMENT_METHODS.split(',')
+    : ['PayPal', 'Stripe', 'CashOnDelivery'];
+
+export const DEFAULT_PAYMENT_METHOD =
+    process.env.DEFAULT_PAYMENT_METHOD || 'PayPal';
+
+// Fetch
 export const LATEST_PRODUCTS_LIMIT =
     Number(process.env.LATEST_PRODUCTS_LIMIT) || 4;
+export const PAGE_SIZE = Number(process.env.PAGE_SIZE) || 2;
 
+// Form default values
 export const signInDefaultValues = {
     email: '',
     password: '',
@@ -19,11 +32,17 @@ export const shippingAddressDefaultValues = {
     country: '',
 };
 
-export const PAYMENT_METHODS = process.env.PAYMENT_METHODS
-    ? process.env.PAYMENT_METHODS.split(',')
-    : ['PayPal', 'Stripe', 'CashOnDelivery'];
-
-export const DEFAULT_PAYMENT_METHOD =
-    process.env.DEFAULT_PAYMENT_METHOD || 'PayPal';
-
-export const PAGE_SIZE = Number(process.env.PAGE_SIZE) || 2;
+export const productDefaultValues = {
+    name: '',
+    slug: '',
+    category: '',
+    images: [],
+    brand: '',
+    description: '',
+    price: '0',
+    stock: 0,
+    rating: '0',
+    numReviews: '0',
+    isFeatured: false,
+    banner: null,
+};
